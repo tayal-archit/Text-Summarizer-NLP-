a901fae583e620bcb124c0ba51602aa0860c9c5d
Archit <atayalwork@gmail.com>
2024-06-05 07:47:43 +0530

    Added Requirements

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-NLP-"
+AUTHOR_USER_NAME = "tayal-archit"
+SRC_REPO = "Text-Summarizer"
+AUTHOR_EMAIL = "atayalwork@gmail.com"
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    # This function will return the list of requirements without '-e .' if it is present 
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+    install_requires = get_requirements("requirements.txt")
+)
